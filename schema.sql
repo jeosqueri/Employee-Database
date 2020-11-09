de529bbed104a4710425556df75b6e976ccdde07
jeosqueri <juliasqueri303@gmail.com>
2020-11-09 12:48:27 -0700

    fixed schema typo, created tables, and imported csvs

--- schema.sql
+++ schema.sql
@@ -1,36 +1,40 @@
 -- Create schema for each CSV file
 
 -- Departments
+--DROP TABLE departments;
+
 CREATE TABLE departments(
-	dep_no VARCHAR(30) PK,
+	dept_no VARCHAR(30) PRIMARY KEY,
 	dept_name VARCHAR(30) NOT NULL
 );
 
 -- Employees
+--DROP TABLE employees CASCADE;
+
 CREATE TABLE employees(
-	emp_no INT PK,
-	birth_data DATE NOT NULL,
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(30) NOT NULL,
+	birth_date DATE NOT NULL,
 	first_name VARCHAR(30) NOT NULL,
 	last_name VARCHAR(30) NOT NULL,
 	sex VARCHAR(10) NOT NULL,
-	hire_date DATE NOT NULL,
-	emp_title_id VARCHAR(30) NOT NULL
+	hire_date DATE NOT NULL
 );
 
 -- Dept manager
 CREATE TABLE dept_manager(
-	dept_no VARCHAR(30) NOT NULL,
-	emp_no INT NOT NULL,
+	dept_no VARCHAR(30),
+	emp_no INT,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-);
+)
 
 -- Dept employees
 CREATE TABLE dept_emp(
-	dept_no VARCHAR(30) NOT NULL,
-	emp_no INT NOT NULL,
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	emp_no INT,
+	dept_no VARCHAR(30),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 -- Salaries
