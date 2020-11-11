765930882fa9bc9b839d7b3f34882407f0ebd583
jeosqueri <juliasqueri303@gmail.com>
2020-11-11 13:05:13 -0700

    made sure queries were working correctly

--- schema.sql
+++ schema.sql
@@ -1,7 +1,6 @@
 -- Create schema for each CSV file
 
 -- Departments
---DROP TABLE departments;
 
 CREATE TABLE departments(
 	dept_no VARCHAR(30) PRIMARY KEY,
@@ -9,7 +8,6 @@ CREATE TABLE departments(
 );
 
 -- Employees
---DROP TABLE employees CASCADE;
 
 CREATE TABLE employees(
 	emp_no INT PRIMARY KEY,
@@ -22,22 +20,25 @@ CREATE TABLE employees(
 );
 
 -- Dept manager
+
 CREATE TABLE dept_manager(
-	dept_no VARCHAR(30),
-	emp_no INT,
+	dept_no VARCHAR(30) NOT NULL,
+	emp_no INT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
-)
+);
 
 -- Dept employees
+
 CREATE TABLE dept_emp(
-	emp_no INT,
-	dept_no VARCHAR(30),
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(30) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
 );
 
 -- Salaries
+
 CREATE TABLE salaries(
 	emp_no INT NOT NULL,
 	salary INT NOT NULL,
@@ -45,8 +46,9 @@ CREATE TABLE salaries(
 );
 
 -- Titles
+
 CREATE TABLE titles(
-	title_id VARCHAR(30) NOT NULL,
+	title_id VARCHAR(30) PRIMARY KEY,
 	title VARCHAR(30) NOT NULL
 );
 
