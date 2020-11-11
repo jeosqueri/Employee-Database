edd27e0b327997be6a27ee8f4b4213b06b268872
jeosqueri <juliasqueri303@gmail.com>
2020-11-10 17:13:38 -0700

    created jupyter notebook for bonus section, created histogram

---
+++ Bonus.sql
@@ -0,0 +1,18 @@
+-- BONUS SECTION
+-- Create new table for jupyter for finding employee salaries
+SELECT e.emp_no, s.salary
+INTO employee_salary
+FROM employees AS e
+JOIN salaries AS s
+ON e.emp_no = s.emp_no;
+
+-- Create new table for jupyter for finding employee salaries by title
+SELECT * FROM employee_salary;
+
+SELECT e.emp_no, e.emp_title_id, s.salary
+INTO employee_titles
+FROM employees AS e
+JOIN salaries AS s
+ON e.emp_no = s.emp_no;
+
+SELECT * FROM employee_titles
